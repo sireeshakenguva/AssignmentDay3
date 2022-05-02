11e8e26518b97fb88621be032283376786f1315d
sireeshakenguva <sireeshakenguva@gmail.com>
2022-05-02 14:26:38 +0530

    One file added

---
+++ Employeewage/src/com/bridgelabs/Employee/FirstProgram.java
@@ -0,0 +1,9 @@
+package com.bridgelabs.Employee;
+
+public class FirstProgram
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to EmployeeWage program");
+    }
+}
