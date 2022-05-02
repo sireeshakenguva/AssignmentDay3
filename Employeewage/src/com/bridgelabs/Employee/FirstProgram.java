e2f2142b0300f299ec9be56b3ca358096d634fff
sireeshakenguva <sireeshakenguva@gmail.com>
2022-05-02 15:21:03 +0530

    Updated Code2

--- Employeewage/src/com/bridgelabs/Employee/FirstProgram.java
+++ Employeewage/src/com/bridgelabs/Employee/FirstProgram.java
@@ -1,19 +1,30 @@
 package com.bridgelabs.Employee;
 
+import java.util.Random;
+
+
 public class FirstProgram
 {
     public static void main(String[] args)
     {
-    	int IS_FULL_TIME = 1;
     	
-    	double empCheck = Math.floor(Math.random() * 10 ) % 2;
-    	
-    	if(empCheck == IS_FULL_TIME) {
-    		System.out.println("Employee is Present");
-    	} else {
-    		System.out.println("Employee is Absent");
-    	}
-    	
-        System.out.println("Welcome to EmployeeWage program");
-    }
+    	        System.out.println("Welcome to EmployeeWage program");
+    	        int is_Fullpresent=1;
+    	        int is_Parttime=2;
+    	        int Wageperhour = 20;
+    	        int Fulldayhour = 8;
+    	        int Parttimehour = 4;
+    	        int Month=20;
+    	        Random random = new Random();
+    	        int Empcheck = random.nextInt(3);
+    	        System.out.println("A random int from 0 to 2: " +  Empcheck);
+    	        if (Empcheck == is_Fullpresent)
+    	        {
+    	            System.out.println("Employee is Present");
+    	            int DailyEmpWage=Wageperhour*Fulldayhour*Month;
+    	                    System.out.println("Employee Daily Wage is " +  DailyEmpWage);
+      }
+    	        
+    	        
+                  }
 }
