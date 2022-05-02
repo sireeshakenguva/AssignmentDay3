4bdd2f15b86a98aa6c98dc08d0b525a2b19debcc
sireeshakenguva <sireeshakenguva@gmail.com>
2022-05-02 14:42:07 +0530

    Updated Code1

--- Employeewage/src/com/bridgelabs/Employee/FirstProgram.java
+++ Employeewage/src/com/bridgelabs/Employee/FirstProgram.java
@@ -4,6 +4,16 @@ public class FirstProgram
 {
     public static void main(String[] args)
     {
+    	int IS_FULL_TIME = 1;
+    	
+    	double empCheck = Math.floor(Math.random() * 10 ) % 2;
+    	
+    	if(empCheck == IS_FULL_TIME) {
+    		System.out.println("Employee is Present");
+    	} else {
+    		System.out.println("Employee is Absent");
+    	}
+    	
         System.out.println("Welcome to EmployeeWage program");
     }
 }
